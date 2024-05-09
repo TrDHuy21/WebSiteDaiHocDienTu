63c12f1a60a31a6dd38c315a979090295bb07747
Tran Duc Huy <vlsvhuy@gmail.com>
2024-05-09 16:59:18 +0700

    insert header and nav for all site

--- WebsiteDaiHocDienTu/src/main/java/com/WebsiteDaiHocDienTu/controller/HomeController.java
+++ WebsiteDaiHocDienTu/src/main/java/com/WebsiteDaiHocDienTu/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.WebsiteDaiHocDienTu.controller;
 
 
+import com.WebsiteDaiHocDienTu.model.dto.UserDTO;
+import com.WebsiteDaiHocDienTu.utils.SecurityUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        UserDTO userDTO = SecurityUtils.getPrinciple();
+
+        model.addAttribute("user", userDTO);
         return "home";
     }
 
